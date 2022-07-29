fc1c3c408ef8db9122c775c4e57d8d4139abe3b1
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-07-29 15:10:46 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,4 @@
+username = "Anais_As_A_Bot" #Name: Anais As a Bot
+password = "WhateverPassword"
+client_id = "NotTheRealKey" #API key: NotTheRealKey
+client_secret = "NotTheRealKey" #Secret key: NotTheRealKey
