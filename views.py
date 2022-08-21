3d505e54d4d373cc3f64df275559d8a113c25e31
AnaisND <109735808+AnaisND@users.noreply.github.com>
2022-08-21 16:33:42 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
-def wordcounter(request):
-    words = request.GET["words"]
-    n = len(words.split())
-    return render(request, "wordcounter.html", {"amount" : n})
+
+def counter(request):
+    text = request.POST['text']
+    amount_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_words})
